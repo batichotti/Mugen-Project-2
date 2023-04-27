83cb7c202b8426360871f3aa63e7aa1d0ec57615
Mateus CohuzEr <52006006+MateusCohuzer@users.noreply.github.com>
2023-04-27 15:56:08 -0300

    fix all Entities and DAOs

--- build/generated-sources/ap-source-output/Entidades/Cor_olhos_.java
+++ build/generated-sources/ap-source-output/Entidades/Cor_olhos_.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-04-25T20:44:50", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-04-25T21:17:15", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Cor_olhos.class)
 public class Cor_olhos_ { 
 
